cfa76f6bc781d5a6589e6ad734ec2935eff2f01e
Min Kyung Kang <mnk918p@gmail.com>
2021-08-06 22:14:07 +0900

    feat: 데이터 삽입
    Co-authored-by: An Hyun Cheol <iborymagic@users.noreply.github.com>

---
+++ src/constants/data.js
@@ -0,0 +1,11 @@
+const animals = require('./animals.js');
+const characters = require('./characters.js');
+const heros = require('./heros.js');
+const monsters = require('./monsters.js');
+
+module.exports.data = {
+  animals,
+  characters,
+  heros,
+  monsters,
+};
